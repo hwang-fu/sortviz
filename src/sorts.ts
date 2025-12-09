13867337eb968a9681bb6d6948952555ed4a0073
hwang-fu <junzhe.hwangfu@gmail.com>
2025-12-09 18:13:07 +0100

    feat: add quick sort algorithm

--- src/sorts.ts
+++ src/sorts.ts
@@ -61,3 +61,36 @@ export async function insertionSort(
     array[j + 1] = key;
   }
 }
+
+export async function quickSort(
+  array: number[],
+  onUpdate: () => void,
+  delay: number = 20
+): Promise<void> {
+  const partition = async (lowIndex: number, highInddex: number): Promise<number> => {
+    const pivot = array[highInddex]!;
+    let i = lowIndex - 1;
+    for (let j = lowIndex; j < highInddex; j++) {
+      if (array[j]! < pivot) {
+        i++;
+        [array[i]!, array[j]!] = [array[j]!, array[i]!];
+        onUpdate();
+        await new Promise((resolve, _reject) => setTimeout(resolve, delay));
+      }
+    }
+    [array[i + 1]!, array[highInddex]!] = [array[highInddex]!, array[i + 1]!];
+    onUpdate();
+    await new Promise((resolve, _reject) => setTimeout(resolve, delay));
+    return i + 1;
+  };
+
+  const qsort = async (lowIndex: number, highIndex: number) => {
+    if (lowIndex < highIndex) {
+      const pivotIndex = await partition(lowIndex, highIndex);
+      await qsort(lowIndex, pivotIndex - 1);
+      await qsort(pivotIndex + 1, highIndex);
+    }
+  };
+
+  await qsort(0, array.length - 1);
+}
